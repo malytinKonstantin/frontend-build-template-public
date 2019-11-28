7df9823ea7b78cd93e501a2ff03db8c0a8c9a0b4
const_malytin <malyytin@gmail.com>
2019-11-28 10:01:09 +0300

    fix

--- src/store/ducks/notifications/actions.ts
+++ src/store/ducks/notifications/actions.ts
@@ -1,6 +1,6 @@
 import { createActions } from 'redux-actions'
 
-export const { showNotification, readNotification } = createActions({
-  SHOW_NOTIFICATION: null,
-  READ_NOTIFICATION: null,
-})
+export const { showNotification, readNotification } = createActions(
+  'SHOW_NOTIFICATION',
+  'READ_NOTIFICATION',
+)
